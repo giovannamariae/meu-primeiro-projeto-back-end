ff3c9aab49726e8c05f73c9c227c3f40b46f4966
giovannamariae <gmeliasc.far@gmail.com>
2024-10-28 21:42:58 -0300

    adiciona a primeira versão do meu projeto

---
+++ oi.js
@@ -0,0 +1,8 @@
+console.log("Olá, mundo!")
+//console.log = comando para escrever coisas na tela do computador , mas não qualquer tela, e sim o terminal onde vc opera o programa.
+//npm- coleções de codigos- biblioteca
+//comando para gerenciar pacotes - npm init -y
+//comando para instalar o pacote espress - npm install express
+// criando o servidor - criar uma pasta - server.js dentro de projeto -backend
+// identificadores  exemplo : const (cosntante- não vai mudar)porta =(atribuições) 3333 (porta)
+// palavras reservadas- identificador especial criado pelo criador do java script exemplo const, break, case....
